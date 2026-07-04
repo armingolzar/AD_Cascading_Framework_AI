e8361d662aefe6865b7f03e7b958627e5b6b35e1
Armin Golzar <armingolzar78@gmail.com>
2026-07-04 19:00:10 +0330

    pomdp_env init is writen

--- rl_engin/pomdp_env.py
+++ rl_engin/pomdp_env.py
@@ -10,7 +10,7 @@ class ClinicalPOMDPEnv():
     charges diagnostic costs, and runs your pre-trained each Stage models.
     """
 
-    def __init__(self, data_path=None, stage1_model_path=None):
+    def __init__(self, data_path=None, stage1_model_path=None, stage2_model_path=None):
         # 1. Locate and load your completed dataset
         if data_path is None:
             self.data_path = os.path.join("..", "data", "stage3_synthetic_data.csv")
@@ -28,7 +28,37 @@ class ClinicalPOMDPEnv():
         self.stage2_cols = ['csf_amyloid_beta', 'csf_ptau', 'plasma_ptau217', 'plasma_nfl', 'apoe4_alleles']
         self.stage3_cols = ['mri_hippocampal_volume', 'mri_ventricle_volume', 'mri_gray_matter_volume']
 
-        # Total observation space: 7 (Stage 1) + 3 (Stage 1 model predictions) + 5 (Stage 2) + 3 (Stage 3) = 18 elements
+        # Total observation space: 7 (Stage 1) + 3 (Stage 1 model predictions) + 5 (Stage 2) + 3 + 3 (Stage 3) = 21 elements
+        self.obs_dim = len(self.stage1_cols) + 3 + len(self.stage2_cols) + 3 + len(self.stage3_cols)
+
+
+        # 3. Clinical Economy
+        self.reward_correct = 10.0
+        self.penalty_incorrect = -15.0
+        self.cost_stage2_blood = -1.5
+        self.cost_stage3_mri = -8.0
+        self.invalid_action_penalty = -20
+
+
+        # 4. Device Management and Model Placeholders
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.stage1_model = self._load_model(stage1_model_path, "Stage_1")
+        self.stage2_model = self._load_model(stage2_model_path, "Stage_2")
+
+
+        # Volatile step state trackers
+        self.current_patient_idx = 0
+        self.current_stage = 1
+        self.patient_row = None
+        self.true_class = None
+
+        # Tracking independent model probability vectors
+        self.stage1_probs = np.zeros(3, dtype=np.float32)
+        self.stage2_probs = np.zeros(3, dtype=np.float32)
+
+
+    
+
 
 
 
